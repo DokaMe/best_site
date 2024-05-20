cea41e54491e3707be73aa8ba67e774c3a5ff279
Daria_N <doranhope@mail.ru>
2024-05-20 18:37:54 +0300

    добавила фонарик

--- frontend_app/src/componets/poemsslide.js
+++ frontend_app/src/componets/poemsslide.js
@@ -1,24 +1,55 @@
 import React from "react";
 import "./css/poemsslide.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function PoemsSlide() {
     const [position, setPosition] = useState({ x: 0, y: 0 });
-    const HandleMouseMove = (event) => {
-        setPosition({ x: event.clientX - 500, y: event.clientY - 500 });
-    };
+    
+
+    useEffect(() => {
+        const handleMouseMove = (event) => {
+            setPosition({
+                x: event.clientX-250,
+                y: event.clientY-250,
+            });
+        };
+
+        window.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+        };
+    }, []);
     return (
-        <div className="container" onMouseMove={HandleMouseMove}>
-            <div className="poems">Poems</div>
-            <div className="overlay"></div>
-            <div
-                className="flashlight"
-                style={{
-                    left: `${position.x}px`,
-                    top: `${position.y}px`,
-                }}
-            ></div>
+        <div className="maindiv">
+            <div className="light" style={{left: `${position.x}px`,top:`${position.y}px`}}></div>
+            <div className="content">
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+                <p>Это текст</p>
+            </div>
+
         </div>
+       
     );
 }
 
