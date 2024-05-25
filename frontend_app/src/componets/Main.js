bd0fa7f0da28a152654d8f98159d6d93cb79cbd3
Daria_N <doranhope@mail.ru>
2024-05-25 19:32:31 +0300

    slider

--- frontend_app/src/componets/Main.js
+++ frontend_app/src/componets/Main.js
@@ -3,13 +3,21 @@ import { render } from "react-dom";
 import { Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import "./css/glavn.css";
+import Slider from "./g";
 
 const Main = () => {
     const navigate = useNavigate();
+    const images = [
+        'https://source.unsplash.com/random/600x400?sig=1',
+        'https://source.unsplash.com/random/600x400?sig=2',
+        'https://source.unsplash.com/random/600x400?sig=3',
+        'https://source.unsplash.com/random/600x400?sig=4',
+      ];
+    
 
     return (
         <div>
-            <div className="container">
+            {/* <div className="container">
                 <div className="paper photo1" onClick={() => navigate("shop")}>
                     <p>Магазин</p>
                 </div>
@@ -40,7 +48,10 @@ const Main = () => {
                 transition={{ type: "spring", stiffness: 400, damping: 17 }}
             >
                 <div className="box-inner"></div>
-            </motion.div>
+            </motion.div> */}
+
+            <Slider images={images}/>
+
         </div>
     );
 };
---
+++ frontend_app/src/componets/g.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./css/slider.css";
+import PoemsSlide from "./poemsslide";
+
+const Slider = ({ images }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const nextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    const prevSlide = () => {
+        setCurrentIndex((prevIndex) =>
+            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
+
+    return (
+        <div className="slider">
+            <button className="prev" onClick={prevSlide}>
+                &#10094;
+            </button>
+            <div
+                className="slider-content"
+                style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+            >
+                
+                    <div className="slide" >
+                      <PoemsSlide />
+                    </div>
+                    <div className="slide" style={{backgroundColor:'green'}}>
+                      2
+                    </div>
+                    <div className="slide" style={{backgroundColor:'yellow'}}>
+                      3
+                    </div>
+            
+            </div>
+            <button className="next" onClick={nextSlide}>
+                &#10095;
+            </button>
+        </div>
+    );
+};
+
+export default Slider;
